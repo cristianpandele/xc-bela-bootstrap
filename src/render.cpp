eefe9f931dd80bb8746c8f7314d69eec46d1ef87
Cristian Pandele <cpande23@student.aau.dk>
2024-09-22 20:49:27 +0000

    Added clang-tidy and clang-format rules and applied them to the codebase.

--- src/render.cpp
+++ src/render.cpp
@@ -1,8 +1,8 @@
 /*
- ____  _____ _        _    
-| __ )| ____| |      / \   
-|  _ \|  _| | |     / _ \  
-| |_) | |___| |___ / ___ \ 
+ ____  _____ _        _
+| __ )| ____| |      / \
+|  _ \|  _| | |     / _ \
+| |_) | |___| |___ / ___ \
 |____/|_____|_____/_/   \_\
 
 The platform for ultra-low latency audio and sensor processing
@@ -14,8 +14,8 @@ Centre for Digital Music at Queen Mary University of London.
 http://www.eecs.qmul.ac.uk/~andrewm
 
 (c) 2016 Augmented Instruments Laboratory: Andrew McPherson,
-	Astrid Bin, Liam Donovan, Christian Heinrichs, Robert Jack,
-	Giulio Moro, Laurel Pardue, Victor Zappi. All rights reserved.
+    Astrid Bin, Liam Donovan, Christian Heinrichs, Robert Jack,
+    Giulio Moro, Laurel Pardue, Victor Zappi. All rights reserved.
 
 The Bela software is distributed under the GNU Lesser General Public License
 (LGPL 3.0), available here: https://www.gnu.org/licenses/lgpl-3.0.txt
@@ -28,26 +28,28 @@ float gFrequency = 440.0;
 float gPhase;
 float gInverseSampleRate;
 
-bool setup(BelaContext *context, void *userData)
+bool setup (BelaContext* context, void* userData)
 {
-	gInverseSampleRate = 1.0 / context->audioSampleRate;
-	gPhase = 0.0;
+    gInverseSampleRate = 1.0 / context->audioSampleRate;
+    gPhase             = 0.0;
 
-	return true;
+    return true;
 }
 
-void render(BelaContext *context, void *userData)
+void render (BelaContext* context, void* userData)
 {
-	for(unsigned int n = 0; n < context->audioFrames; n++) {
-		float out = 0.8f * sinf(gPhase);
-		gPhase += 2.0f * (float)M_PI * gFrequency * gInverseSampleRate;
-		if(gPhase > M_PI)
-			gPhase -= 2.0f * (float)M_PI;
+    for (unsigned int n = 0; n < context->audioFrames; n++)
+    {
+        float out = 0.8f * sinf(gPhase);
+        gPhase += 2.0f * (float)M_PI * gFrequency * gInverseSampleRate;
+        if (gPhase > M_PI)
+            gPhase -= 2.0f * (float)M_PI;
 
-		for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
-			audioWrite(context, n, channel, out);
-		}
-	}
+        for (unsigned int channel = 0; channel < context->audioOutChannels; channel++)
+        {
+            audioWrite(context, n, channel, out);
+        }
+    }
 }
 
 void cleanup(BelaContext *context, void *userData)
